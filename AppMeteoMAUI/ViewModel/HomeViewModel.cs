5105b88385d4e30b07d7a556b7ba78b706736416
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-03-23 15:15:30 +0100

    cod sorgente menu item

---
+++ AppMeteoMAUI/ViewModel/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace AppMeteoMAUI.ViewModel
+{
+    public partial class HomeViewModel
+    {
+        [RelayCommand]
+        static async void Cod()
+        {
+            await Launcher.Default.OpenAsync("https://github.com/GiorgioCitterio/AppMeteoMAUI");
+        }
+    }
+}
