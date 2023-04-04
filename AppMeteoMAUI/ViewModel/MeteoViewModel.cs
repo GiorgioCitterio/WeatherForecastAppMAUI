17c196daeede9931c417bc3157bbabba0f0662ca
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-04-04 13:38:04 +0200

    commit

--- AppMeteoMAUI/ViewModel/MeteoViewModel.cs
+++ AppMeteoMAUI/ViewModel/MeteoViewModel.cs
@@ -102,6 +102,14 @@ namespace AppMeteoMAUI.ViewModel
         }
         #endregion
 
+        #region MainPage
+        [RelayCommand]
+        private async Task BackToMainPage()
+        {
+            await App.Current.MainPage.Navigation.PopToRootAsync();
+        }
+        #endregion
+
         #region Geolocalizzazione
 
         [RelayCommand]
